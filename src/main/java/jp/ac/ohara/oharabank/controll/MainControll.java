525329462fde88235fa563616a0980f42e230479
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-03-15 15:37:41 +0900

    seisaku

---
+++ src/main/java/jp/ac/ohara/oharabank/controll/MainControll.java
@@ -0,0 +1,14 @@
+package jp.ac.ohara.oharabank.controll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainControll {
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("message", "こんにちは");
+    return "top";
+  }
+}
